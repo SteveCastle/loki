69503999595a7ef7d9a8da05d787f9afdb65a57d
Stephen Castle <stephen@callsignmedia.com>
2020-09-12 18:37:52 -0700

    update version in about

--- src/About.js
+++ src/About.js
@@ -21,7 +21,7 @@ function About({ setAbout }) {
           </span>
           <h1>LowKey Image Viewer</h1>
         </div>
-        <span className="version">Version: 1.0.6</span>
+        <span className="version">Version: 1.0.7</span>
         <span>
           <a
             href=""
