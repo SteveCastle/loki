28970f018a06d45eead481d202ef07e083d0f40a
Stephen Castle <stephen@callsignmedia.com>
2021-04-09 17:59:53 -0700

    fix crash when dir contains permissions error

--- src/loadImageList.js
+++ src/loadImageList.js
@@ -1,6 +1,7 @@
 var memoize = window.require("memoizee");
 const readdir = window.require("readdir-enhanced");
 const path = window.require("path");
+const fs = window.require("fs");
 
 const sorts = {
   CREATE_DATE: (a, b) => b.modified - a.modified,
@@ -14,23 +15,40 @@ export default async function loadImageList(
   recursive = false
 ) {
   console.log(folderPath);
+  let items, sortedItems;
   // Use readdir-enhanced to get a list of files at a path given the settings passed in.
-  let items = await readdir.async(folderPath, {
-    filter,
-    deep: recursive,
-    basePath: folderPath,
-    stats: true,
-  });
-
-  // Do not include hidden items, then map to object with uri and date.
-  // Finally sort by the selected sort key.
-  let sortedItems = items
-    .filter((item) => path.basename(item.path)[0] !== ".")
-    .map((item) => ({
-      fileName: item.path,
-      modified: item.mtimeMs,
-    }))
-    .sort(sorts[sortOrder]);
+  try {
+    items = await readdir.async(folderPath, {
+      filter,
+      deep: recursive,
+      basePath: folderPath,
+      stats: true,
+    });
+    // Do not include hidden items, then map to object with uri and date.
+    // Finally sort by the selected sort key.
+    sortedItems = items
+      .filter((item) => path.basename(item.path)[0] !== ".")
+      .map((item) => ({
+        fileName: item.path,
+        modified: item.mtimeMs,
+      }))
+      .sort(sorts[sortOrder]);
+  } catch (err) {
+    items = await readdir.async(folderPath, {
+      filter,
+      deep: recursive,
+      basePath: folderPath,
+    });
+    // Do not include hidden items, then map to object with uri and date.
+    // Finally sort by the selected sort key.
+    sortedItems = items
+      .filter((item) => path.basename(item)[0] !== ".")
+      .map((item) => ({
+        fileName: item,
+        modified: fs.statSync(item).mtimeMs,
+      }))
+      .sort(sorts[sortOrder]);
+  }
 
   return { items: sortedItems };
 }
