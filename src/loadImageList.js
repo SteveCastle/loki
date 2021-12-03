ee5e5097b32207c132ba5298d01b50447bb65c0a
Stephen Castle <stephen@callsignmedia.com>
2021-12-03 15:36:42 -0800

    initial db setup

--- src/loadImageList.js
+++ src/loadImageList.js
@@ -1,53 +1,15 @@
-const naturalCompare = window.require("natural-compare-lite");
-const readdir = window.require("readdir-enhanced");
-const path = window.require("path");
-const fs = window.require("fs");
-const sorts = {
-  CREATE_DATE: (a, b) => b.modified - a.modified,
-  ALPHA: (a, b) =>
-    naturalCompare(a.fileName.toLowerCase(), b.fileName.toLowerCase()),
-};
+const knex = window.require("knex")({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "sparrow",
+    password: "sparrow",
+    database: "sparrow",
+  },
+});
 
-export default async function loadImageList(
-  folderPath,
-  filter,
-  sortOrder,
-  recursive = false
-) {
-  console.log("Loading Images from:", folderPath);
-  let items, sortedItems;
-  // Use readdir-enhanced to get a list of files at a path given the settings passed in.
-  try {
-    items = await readdir.async(folderPath, {
-      filter,
-      deep: recursive,
-      basePath: folderPath,
-      stats: true,
-    });
-    // Do not include hidden items, then map to object with uri and date.
-    // Finally sort by the selected sort key.
-    sortedItems = items
-      .filter((item) => path.basename(item.path)[0] !== ".")
-      .map((item) => ({
-        fileName: item.path,
-        modified: item.mtimeMs,
-      }))
-      .sort(sorts[sortOrder.key]);
-  } catch (err) {
-    items = await readdir.async(folderPath, {
-      filter,
-      deep: recursive,
-      basePath: folderPath,
-    });
-    // Do not include hidden items, then map to object with uri and date.
-    // Finally sort by the selected sort key.
-    sortedItems = items
-      .filter((item) => path.basename(item)[0] !== ".")
-      .map((item) => ({
-        fileName: item,
-        modified: fs.statSync(item).mtimeMs,
-      }))
-      .sort(sorts[sortOrder.key]);
-  }
-  return { items: sortedItems };
+export default async function loadImageList() {
+  const items = await knex().select("*").from("things")
+  return { items: items.map((item) => ({ fileName: item.uri, modified: 0 })) };
 }
