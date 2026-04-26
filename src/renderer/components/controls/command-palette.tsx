0f0b2408c7af4cfc3775bc72df75047fd1deff1f
Steve Castle <sc@elf.pub>
2026-04-26 08:05:54 -0700

    chore: bump version to 2.8.2

--- src/renderer/components/controls/command-palette.tsx
+++ src/renderer/components/controls/command-palette.tsx
@@ -478,7 +478,7 @@ const VersionInfo: React.FC = React.memo(() => {
     }
   }, []);
 
-  const currentVersion = result?.currentVersion || '2.8.1';
+  const currentVersion = result?.currentVersion || '2.8.2';
 
   return (
     <div className="versionContainer">
